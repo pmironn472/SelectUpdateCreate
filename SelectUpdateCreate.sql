f7e4753b8b4b08d40515877230d5994595da3896
pmironn472 <63416798+pmironn472@users.noreply.github.com>
2020-08-20 15:53:50 +0300

    Update SelectUpdateCreate.sql

--- SelectUpdateCreate.sql
+++ SelectUpdateCreate.sql
@@ -1,3 +1,41 @@
 UPDATE public.categoires
   SET name  = 'Dogs T-shirts'
   WHERE id = 3 ; 
+  
+UPDATE public.products
+ SET namess = 'Mario' , "quantity " = 85
+ WHERE id = 1 ; 
+  
+ALTER TABLE public.products
+ RENAME COLUMN name TO namess;
+
+SELECT * FROM public.products;
+
+SELECT * FROM public.categoires;
+
+SELECT * FROM public.clients;
+
+
+DELETE from public.products WHERE id = 2;
+
+INSERT INTO public.clients(
+	id,"full_name","phone","email"
+) VALUES (
+'2'::integer,'Nirca Andrei'::text,'079856215'::text,'magmail@me.com'::text
+)
+returning id;
+
+
+CREATE TABLE public.clients
+(
+	id integer NOT NULL,
+	full_name text COLLATE pg_catalog."default",
+	phone text COLLATE pg_catalog."default",
+	email text COLLATE pg_catalog."default",
+	CONSTRAINT clients_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.clients
+OWNER to postgres;
